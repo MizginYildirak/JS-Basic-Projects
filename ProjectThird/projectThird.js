2b41c288243a30f5498bb1214bac39e505147875
MizginYildirak <mizgin_yldrak@hotmail.com>
2023-06-08 00:47:00 +0300

    add filter project

--- ProjectThird/projectThird.js
+++ ProjectThird/projectThird.js
@@ -1,7 +1,43 @@
-const companyBtn = document.querySelectorAll(".company-btn")
-const productsContainer = document.querySelector(".products-container")
-const product = document.querySelectorAll(".product")
+const companyBtn = document.querySelectorAll(".company-btn");
+const productsContainer = document.querySelector(".products-container");
+const searchInput = document.querySelector(".search-input");
+const product = document.querySelectorAll(".product");
+const productName = document.querySelectorAll(".product-name");
 
-const products = Array.from(product)
+for (let i = 0; i < companyBtn.length; i++) {
+    companyBtn[i].addEventListener("click", (event) => {
+        const dataId = event.currentTarget.getAttribute("data-id");
 
-console.log(products)
+        switch (dataId) {
+            case "all":
+                Array.from(product).forEach(item => {
+                    item.style.display = "block";
+                });
+                break;
+            default:
+                Array.from(product).forEach(item => {
+                    if (item.getAttribute("data-id") === dataId) {
+                        item.style.display = "block";
+                    } else {
+                        item.style.display = "none";
+                    }
+                });
+                break;
+        }
+    });
+}
+
+searchInput.onkeyup = () => {
+    let search = searchInput.value.toLowerCase();
+
+    Array.from(productName).forEach(item => {
+        let el = item.innerHTML.toLowerCase();
+        let productItem = item.closest('.product');
+
+        if (el.indexOf(search) === -1) {
+            productItem.style.display = "none";
+        } else {
+            productItem.style.display = "block";
+        }
+    });
+};
