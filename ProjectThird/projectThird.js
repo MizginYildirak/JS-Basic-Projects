5d8e47e6e1585c9a3c663bf38a675bf471f10f8d
MizginYildirak <mizgin_yldrak@hotmail.com>
2023-06-07 04:22:01 +0300

    add projectThird

---
+++ ProjectThird/projectThird.js
@@ -0,0 +1,7 @@
+const companyBtn = document.querySelectorAll(".company-btn")
+const productsContainer = document.querySelector(".products-container")
+const product = document.querySelectorAll(".product")
+
+const products = Array.from(product)
+
+console.log(products)
