7e656e95857cc38a569677616abe620c422cd001
MizginYildirak <mizgin_yldrak@hotmail.com>
2023-06-11 23:41:06 +0300

    add product details

--- Products Project/app.js
+++ Products Project/app.js
@@ -2,66 +2,78 @@ const products = [
   {
     image: "https://course-api.com/images/store/product-1.jpeg",
     name: "High-Back Bench",
-    price: "$9.99",
+    subTitle: "Ikea",
+    price: "$9.99"
   },
   {
     image: "https://course-api.com/images/store/product-2.jpeg",
     name: "Albany Table",
-    price: "$79.99",
+    subTitle: "Ikea",
+    price: "$79.99"
   },
   {
     image: "https://course-api.com/images/store/product-3.jpeg",
     name: "Accent Chair",
-    price: "$25.99",
+    subTitle: "Ikea",
+    price: "$25.99"
   },
   {
     image: "https://course-api.com/images/store/product-4.jpeg",
     name: "Wooden Table",
-    price: "$45.99",
+    subTitle: "Ikea",
+    price: "$45.99"
   },
   {
     image: "https://course-api.com/images/store/product-5.jpeg",
     name: "Dining Table",
-    price: "$6.99",
+    subTitle: "Ikea",
+    price: "$6.99"
   },
   {
     image: "https://course-api.com/images/store/product-6.jpeg",
     name: "Sofa Set",
-    price: "$69.99",
+    subTitle: "Ikea",
+    price: "$69.99"
   },
   {
     image: "https://course-api.com/images/store/product-7.jpeg",
     name: "Modern Bookshelf",
-    price: "$8.99",
+    subTitle: "Ikea",
+    price: "$8.99"
   },
   {
     image: "https://course-api.com/images/store/product-8.jpeg",
     name: "Emperor Bed",
-    price: "$21.99",
+    subTitle: "Ikea",
+    price: "$21.99"
   },
   {
     image: "https://course-api.com/images/store/product-9.jpeg",
     name: "Utopia Sofa",
-    price: "$39.99",
+    subTitle: "Ikea",
+    price: "$39.99"
   },
   {
     image: "https://course-api.com/images/store/product-10.jpeg",
     name: "Entertainment Center",
-    price: "$29.98",
+    subTitle: "Ikea",
+    price: "$29.98"
   },
   {
     image: "https://course-api.com/images/store/product-11.jpeg",
     name: "Albany Sectional",
-    price: "$10.99",
+    subTitle: "Ikea",
+    price: "$10.99"
   },
   {
     image: "https://course-api.com/images/store/product-12.jpeg",
     name: "Leather Sofa",
-    price: "$9.99",
+    subTitle: "Ikea",
+    price: "$9.99"
   },
 ];
 
-const productsContainer = document.querySelector(".products-container");
+const productsContainer = document.querySelector(".products-container")
 
 if (productsContainer) {
   products.forEach((item) => {
@@ -73,26 +85,32 @@ if (productsContainer) {
             <span>${item.price}</span>
         </footer>
     </a>
-`;
-  });
+`
+  })
 }
 
 const getProduct = () => {
-  const singleProduct = document.querySelector(".single-product");
-  const productImage = document.querySelector(".product-image");
+  const singleProduct = document.querySelector(".single-product")
+  const productImage = document.querySelector(".product-image")
 
-  const params = new URLSearchParams(window.location.search);
-  const id = params.get("productId");
+  const params = new URLSearchParams(window.location.search)
+  const id = params.get("productId")
 
   if (id) {
-    const productDetail = products.find((item) => item.name === id);
+    const price = document.querySelector(".price")
+    const productTitle = document.querySelector(".product-title")
+    const subTitle = document.querySelector(".sub-title")
+    const productDetail = products.find((item) => item.name === id)
     if (productDetail) {
-      productImage.src = productDetail.image;
+      productImage.src = productDetail.image
+      price.innerHTML = productDetail.price
+      productTitle.innerHTML = productDetail.name
+      subTitle.innerHTML = productDetail.subTitle
     }
   }
-};
+}
 
-getProduct();
+getProduct()
 
 // const attachEventListeners = () => {
 //     const singleProducts = document.querySelectorAll(".single-product");
