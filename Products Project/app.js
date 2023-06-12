6d764780159ee6da9121ccd8fa5fe1137bc22bfc
MizginYildirak <mizgin_yldrak@hotmail.com>
2023-06-13 02:29:25 +0300

    fix products id

--- Products Project/app.js
+++ Products Project/app.js
@@ -1,84 +1,96 @@
 const products = [
   {
+    id: 1,
     image: "https://course-api.com/images/store/product-1.jpeg",
     name: "High-Back Bench",
     subTitle: "Ikea",
-    price: "$9.99"
+    price: "$9.99",
   },
   {
+    id: 2,
     image: "https://course-api.com/images/store/product-2.jpeg",
     name: "Albany Table",
-    subTitle: "Ikea",
-    price: "$79.99"
+    subTitle: "Marcos",
+    price: "$79.99",
   },
   {
+    id: 3,
     image: "https://course-api.com/images/store/product-3.jpeg",
     name: "Accent Chair",
-    subTitle: "Ikea",
-    price: "$25.99"
+    subTitle: "Caressa",
+    price: "$25.99",
   },
   {
+    id: 4,
     image: "https://course-api.com/images/store/product-4.jpeg",
     name: "Wooden Table",
-    subTitle: "Ikea",
-    price: "$45.99"
+    subTitle: "Caressa",
+    price: "$45.99",
   },
   {
+    id: 5,
     image: "https://course-api.com/images/store/product-5.jpeg",
     name: "Dining Table",
-    subTitle: "Ikea",
-    price: "$6.99"
+    subTitle: "Caressa",
+    price: "$6.99",
   },
   {
+    id: 6,
     image: "https://course-api.com/images/store/product-6.jpeg",
     name: "Sofa Set",
-    subTitle: "Ikea",
-    price: "$69.99"
+    subTitle: "Liddy",
+    price: "$69.99",
   },
   {
+    id: 7,
     image: "https://course-api.com/images/store/product-7.jpeg",
     name: "Modern Bookshelf",
-    subTitle: "Ikea",
-    price: "$8.99"
+    subTitle: "Marcos",
+    price: "$8.99",
   },
   {
+    id: 8,
     image: "https://course-api.com/images/store/product-8.jpeg",
     name: "Emperor Bed",
-    subTitle: "Ikea",
-    price: "$21.99"
+    subTitle: "Liddy",
+    price: "$21.99",
   },
   {
+    id: 9,
     image: "https://course-api.com/images/store/product-9.jpeg",
     name: "Utopia Sofa",
-    subTitle: "Ikea",
-    price: "$39.99"
+    subTitle: "Marcos",
+    price: "$39.99",
   },
   {
+    id: 10,
     image: "https://course-api.com/images/store/product-10.jpeg",
     name: "Entertainment Center",
-    subTitle: "Ikea",
-    price: "$29.98"
+    subTitle: "Liddy",
+    price: "$29.98",
   },
   {
+    id: 11,
     image: "https://course-api.com/images/store/product-11.jpeg",
     name: "Albany Sectional",
     subTitle: "Ikea",
-    price: "$10.99"
+    price: "$10.99",
   },
   {
+    id: 12,
     image: "https://course-api.com/images/store/product-12.jpeg",
     name: "Leather Sofa",
-    subTitle: "Ikea",
-    price: "$9.99"
+    subTitle: "Liddy",
+    price: "$9.99",
   },
-];
+]
 
 const productsContainer = document.querySelector(".products-container")
 
 if (productsContainer) {
   products.forEach((item) => {
     productsContainer.innerHTML += `
-    <a class="single-product" href="product.html?productId=${item.name}">
+    <a class="single-product" href="product.html?productId=${item.id}">
         <img class="single-product-img img" id="photo" src="${item.image}">
         <footer>
             <h5>${item.name}</h5>
@@ -90,17 +102,16 @@ if (productsContainer) {
 }
 
 const getProduct = () => {
-  const singleProduct = document.querySelector(".single-product")
   const productImage = document.querySelector(".product-image")
+  const price = document.querySelector(".price")
+  const productTitle = document.querySelector(".product-title")
+  const subTitle = document.querySelector(".sub-title")
 
-  const params = new URLSearchParams(window.location.search)
-  const id = params.get("productId")
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get("productId");
 
   if (id) {
-    const price = document.querySelector(".price")
-    const productTitle = document.querySelector(".product-title")
-    const subTitle = document.querySelector(".sub-title")
-    const productDetail = products.find((item) => item.name === id)
+    const productDetail = products.find((item) => item.id == id)
     if (productDetail) {
       productImage.src = productDetail.image
       price.innerHTML = productDetail.price
@@ -111,16 +122,3 @@ const getProduct = () => {
 }
 
 getProduct()
-
-// const attachEventListeners = () => {
-//     const singleProducts = document.querySelectorAll(".single-product");
-
-//     singleProducts.forEach((product, index) => {
-//         product.addEventListener("click", (event) => {
-//             const productImage = document.querySelector(".product-image");
-//             console.log(producs)
-//             productImage.src = products[index].product;
-
-//         });
-//     });
-// };
