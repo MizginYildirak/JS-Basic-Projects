c48f9e73ca3f001514fe8e8a6d1dd49dd1e66380
MizginYildirak <mizgin_yldrak@hotmail.com>
2023-12-17 09:31:08 +0300

    add checked toggles to lists

--- combobox/app.js
+++ combobox/app.js
@@ -1,7 +1,21 @@
 const selectBoxBtn = document.querySelector(".select-box")
 const searchBox = document.querySelector(".search-box")
+const listItem = document.querySelectorAll(".list-item")
 
 function revealSearchBox(e) {
     e.classList.toggle("style-active")
     searchBox.style.display = "flex"
 }
+
+function listItemChecked() {
+    for (let i = 0; i < listItem.length; i++) {
+        listItem[i].addEventListener("click", function() {
+            console.log("hello")
+            listItem[i].appendChild("i")
+            listItem[i].classList.toggle('fas');
+            listItem[i].classList.toggle('fa-check'); 
+        })
+    }
+}
+
+console.log(listItemChecked())
