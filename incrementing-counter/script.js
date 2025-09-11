d55f015b6dcd59b9d5f94d6eecb11747546b57be
Mizgin Yıldırak <mizgin_yldrak@hotmail.com>
2025-09-11 13:00:02 +0300

    finish incrementing-counter

---
+++ incrementing-counter/script.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const increment = target / 200;
+
+    const c = +counter.innerText;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
